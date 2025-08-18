ab78ab5a6445347f7bad342da10742d6b1248b16
Rohit-Pakhre <pakhrerohit281@gmail.com>
2025-08-18 06:13:20 +0530

    Feat: Files and Folder added.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black/90">
+      <p className="text-white">App</p>
+    </main>
+  );
+};
+
+export default App;
