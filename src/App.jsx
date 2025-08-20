25b5cfb9686894b9df27f4323de2f71868357648
salonifojdar27 <salonifojdar27@gmail.com>
2025-08-20 11:47:10 +0530

    Fix: Conflict Solved.

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ const App = () => {
   const { lightTheme } = useContext(AppContext);
   return (
     <main
-      className={`min-h-screen flex items-center ${lightTheme ? "bg-black/90" : "bg-white"
+      className={`min-h-screen flex items-center ${lightTheme ? "bg-black/90" : "bg-gray-200"
         } transition-all duration-300 ease-in-out`}
     >
       <section className="min-h-screen flex flex-1 items-center justify-between">
         <Sidebar />
         <Navbar />
       </section>
-F    </main>
+   </main>
   );
 };
 
